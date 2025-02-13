efa937d5a9df37b5c237833ef76eca4eac34d7af
Blake Downward <54337911+blakedownward@users.noreply.github.com>
2025-02-13 16:52:29 +1030

    make the basic usage script more helpful

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -4,15 +4,15 @@ from pyletteyes.colour import Colour
 red = Colour.from_hex("#FF0000")
 print(red)
 
-# Lighten it and output as hex
+# Lighten it and output as HSL values
 light_red = red.lighten(0.1)
 print(light_red.hsl)
 
-# Darken it and output as HSL values
+# Darken it and print as an RGB string
 dark_red = red.darken(0.2)
-print(dark_red.hsl)
+print(dark_red)
 
-# Get complementary colour
+# Get complementary colour and output as hex
 complement = red.get_complementary()
-print(complement)
+print(complement.to_hex())
 
